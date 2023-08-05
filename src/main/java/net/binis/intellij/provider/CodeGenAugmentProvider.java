35032c384d8289173b152a1cdaa0f3587afd09a5
Binis <binisbelev@gmail.com>
2023-08-05 10:04:45 +0300

    Initial Enricher augmenting

--- src/main/java/net/binis/intellij/provider/CodeGenAugmentProvider.java
+++ src/main/java/net/binis/intellij/provider/CodeGenAugmentProvider.java
@@ -4,16 +4,23 @@ import com.intellij.lang.java.JavaLanguage;
 import com.intellij.openapi.util.Key;
 import com.intellij.psi.*;
 import com.intellij.psi.augment.PsiAugmentProvider;
+import com.intellij.psi.impl.light.LightFieldBuilder;
+import com.intellij.psi.impl.light.LightModifierList;
 import com.intellij.psi.impl.source.PsiAnnotationMethodImpl;
 import net.binis.intellij.tools.Binis;
+import net.binis.intellij.tools.Lookup;
+import net.binis.intellij.tools.objects.EnricherData;
+import org.apache.commons.lang3.StringUtils;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import java.lang.reflect.Modifier;
 import java.util.*;
 
 import static com.intellij.openapi.util.NullUtils.notNull;
 import static java.util.Objects.isNull;
 import static java.util.Objects.nonNull;
+import static net.binis.codegen.tools.Tools.with;
 
 public class CodeGenAugmentProvider extends PsiAugmentProvider {
 
@@ -34,11 +41,11 @@ public class CodeGenAugmentProvider extends PsiAugmentProvider {
     public <Psi extends PsiElement> List<Psi> getAugments(@NotNull PsiElement element,
                                                           @NotNull final Class<Psi> type,
                                                           @Nullable String nameHint) {
-        var result = Collections.<PsiElement>emptyList();
+        List<PsiElement> result = new ArrayList<>();
         if (element instanceof PsiClass cls && cls.getLanguage().isKindOf(JavaLanguage.INSTANCE)) {
             if (cls.isAnnotationType()) {
                 if (PsiMethod.class.equals(type) && notNull(cls.getExtendsList()) && Binis.isAnnotationInheritanceEnabled(element.getProject())) {
-                    var methods = new ArrayList<PsiMethod>();
+                    List<PsiMethod> methods = (List) result;
                     var list = cls.getExtendsList().getReferencedTypes();
                     if (list.length > 0) {
                         Arrays.stream(list)
@@ -50,15 +57,14 @@ public class CodeGenAugmentProvider extends PsiAugmentProvider {
                                                 .filter(m -> isNull(m.getUserData(AUGMENTED)))
                                                 .filter(m -> methods.stream().noneMatch(a ->
                                                         a.getName().equals(m.getName())))
+                                                //TODO: Use LightMethodBuilder
                                                 .map(PsiElement::copy)
                                                 .map(PsiMethod.class::cast)
                                                 .forEach(m -> {
                                                     m.putUserData(AUGMENTED, m.getName());
                                                     methods.add(m);
                                                 }));
-                        result = (List) methods;
                         cls.putUserData(AUGMENTED, cls.getQualifiedName());
-
                     }
                 }
             } else {
@@ -71,16 +77,14 @@ public class CodeGenAugmentProvider extends PsiAugmentProvider {
                         }
                         augmenting.add(cls);
                         try {
-                            var fields = new ArrayList<PsiField>();
                             Arrays.stream(cls.getAllMethods())
                                     .filter(m -> !m.hasParameters())
                                     .filter(m -> nonNull(m.getReturnType()))
                                     .map(this::createField)
                                     .forEach(m -> {
                                         m.putUserData(AUGMENTED, m.getName());
-                                        fields.add(m);
+                                        result.add(m);
                                     });
-                            result = (List) fields;
                             cls.putUserData(AUGMENTED, cls.getQualifiedName());
                         } finally {
                             augmenting.remove(cls);
@@ -91,15 +95,81 @@ public class CodeGenAugmentProvider extends PsiAugmentProvider {
                     }
                 }
             }
+            checkForAnnotationAugments(result, cls, type);
         }
 
         return (List) result;
     }
 
+    @SuppressWarnings("unchecked")
+    private void checkForAnnotationAugments(List<PsiElement> result, PsiClass cls, Class<? extends PsiElement> type) {
+        with(Lookup.getPrototypeData(cls), proto ->
+                with((List<EnricherData>) proto.getCustom().get("enrichers"), list ->
+                    list.stream()
+                            .filter(data -> filterByType(data, type))
+                            .forEach(data -> {
+                                switch (data.getAdds()) {
+                                    case FIELD -> {
+                                        if (StringUtils.isNotBlank(data.getName()) && StringUtils.isNotBlank(data.getType())) {
+                                            var field = createField(cls, data);
+                                            field.putUserData(AUGMENTED, field.getName());
+                                            result.add(field);
+                                        }
+                                    }
+                                    case METHOD -> {
+                                        //Not implemented
+                                    }
+                                    case CONSTRUCTOR -> {
+                                        //Not implemented
+                                    }
+                                }
+                            })
+
+                ));
+    }
+
+    private boolean filterByType(EnricherData data, Class<? extends PsiElement> type) {
+        return switch (data.getAdds()) {
+            case FIELD -> PsiField.class.isAssignableFrom(type);
+            case METHOD -> PsiMethod.class.isAssignableFrom(type) && !PsiMethod.class.cast(type).isConstructor();
+            case CONSTRUCTOR -> PsiMethod.class.isAssignableFrom(type) && PsiMethod.class.cast(type).isConstructor();
+            default -> false;
+        };
+    }
+
     protected PsiField createField(PsiMethod method) {
+        //TODO: Use LightFieldBuilder
         var factory = JavaPsiFacade.getElementFactory(method.getProject());
         var field = factory.createField(method.getName(), method.getReturnType());
         field.getModifierList().setModifierProperty(PsiModifier.PUBLIC, true);
         return field;
     }
+
+    protected PsiField createField(PsiClass cls, EnricherData data) {
+        var manager = cls.getContainingFile().getManager();
+        var list = new LightModifierList(manager);
+        var field = new LightFieldBuilder(data.getName(), data.getType(), cls).setModifierList(list);
+        field.setContainingClass(cls);
+        handleModifiers(list, data.getModifier());
+        return field;
+    }
+
+    private void handleModifiers(LightModifierList builder, long modifier) {
+        if ((modifier & Modifier.PUBLIC) != 0) {
+            builder.addModifier(PsiModifier.PUBLIC);
+        }
+        if ((modifier & Modifier.PRIVATE) != 0) {
+            builder.addModifier(PsiModifier.PRIVATE);
+        }
+        if ((modifier & Modifier.PROTECTED) != 0) {
+            builder.addModifier(PsiModifier.PROTECTED);
+        }
+        if ((modifier & Modifier.STATIC) != 0) {
+            builder.addModifier(PsiModifier.STATIC);
+        }
+        if ((modifier & Modifier.FINAL) != 0) {
+            builder.addModifier(PsiModifier.FINAL);
+        }
+    }
+
 }
