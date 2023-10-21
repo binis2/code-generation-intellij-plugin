99ffd1cbd6e4955b6428897f343b34c6b7092eec
Binis <binisbelev@gmail.com>
2023-10-21 11:24:23 +0300

    release/1.1.13

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "net.binis.intellij"
-version = "1.1.12"
+version = "1.1.13"
 
 repositories {
     mavenLocal()
@@ -13,7 +13,7 @@ repositories {
 }
 
 dependencies {
-    implementation("dev.binis:code-generator:1.1.12")
+    implementation("dev.binis:code-generator:1.1.13")
 }
 
 // Configure Gradle IntelliJ Plugin
