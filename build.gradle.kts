c6b287967bb52b9d7ce91a139331d6e241c10619
Binis <binisbelev@gmail.com>
2025-04-21 12:42:45 +0300

    Updated for idea 2025.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,49 +1,40 @@
 plugins {
     id("java")
-    id("org.jetbrains.intellij") version "1.16.1"
-    id("io.freefair.lombok") version "5.3.3.3"
+    id("org.jetbrains.intellij.platform") version "2.4.0"
+    id("io.freefair.lombok") version "8.13.1"
 }
 
 group = "net.binis.intellij"
-version = "1.2.22"
+version = "1.2.23.1"
 
 repositories {
     mavenLocal()
     mavenCentral()
+
+    intellijPlatform {
+        defaultRepositories()
+    }
 }
 
 dependencies {
-    implementation("dev.binis:code-generator:1.2.22")
+    implementation("dev.binis:code-generator:1.2.23")
+    intellijPlatform {
+        intellijIdeaCommunity("2025.1")
+        bundledPlugin("com.intellij.java")
+    }
 }
 
-// Configure Gradle IntelliJ Plugin
-// Read more: https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
-intellij {
-    version.set("2024.2")
-    type.set("IC") // Target IDE Platform
+intellijPlatform {
+    buildSearchableOptions = false
 
-    plugins.set(listOf("com.intellij.java"))
-}
-
-tasks {
-    // Set the JVM compatibility versions
-    withType<JavaCompile> {
-        sourceCompatibility = "17"
-        targetCompatibility = "17"
+    pluginConfiguration {
+        ideaVersion {
+            sinceBuild = "251"
+        }
     }
-
-    patchPluginXml {
-        sinceBuild.set("242")
-        untilBuild.set("242.*")
-    }
-
-    signPlugin {
-        certificateChain.set(System.getenv("CERTIFICATE_CHAIN"))
-        privateKey.set(System.getenv("PRIVATE_KEY"))
-        password.set(System.getenv("PRIVATE_KEY_PASSWORD"))
-    }
-
-    publishPlugin {
-        token.set(System.getenv("PUBLISH_TOKEN"))
+    pluginVerification  {
+        ides {
+            recommended()
+        }
     }
 }
